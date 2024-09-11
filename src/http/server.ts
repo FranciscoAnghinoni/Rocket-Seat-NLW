8a3dbf0ace878a6518dca31437655f8233315225
FRANCISCO ANGHINONI <102160768+FranciscoAnghinoni@users.noreply.github.com>
2024-09-11 18:42:32 -0400

    feat: Add fastify-type-provider-zod package
    This commit adds the "fastify-type-provider-zod" package to the project dependencies. This package provides type validation and serialization for Fastify routes using Zod schemas. It also adds the necessary imports and configuration in the "server.ts" file to enable type validation and serialization.
    
    The "fastify-type-provider-zod" package is added with version 2.0.0, and it has a peer dependency on Fastify version 4.0.0 and Zod version 3.14.2.
    
    This change is necessary to enhance the type safety and validation of the API routes in the project.

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,64 @@
 import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
 
-const app = fastify()
+import { createGoal } from '../services/create-goal'
+import z from 'zod'
+import { getWeekPendingGoals } from '../services/get-week-pending-goals'
+import { createGoalCompletion } from '../services/create-goal-completion'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server is running on port 3333')
 })
 
+app.get('/pending-goals', async () => {
+  const { pendingGoals } = await getWeekPendingGoals()
+
+  return { pendingGoals }
+})
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const { title, desiredWeeklyFrequency } = request.body
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
+
+app.post(
+  '/completions',
+  {
+    schema: {
+      body: z.object({
+        goalId: z.string(),
+      }),
+    },
+  },
+  async request => {
+    const { goalId } = request.body
+
+    const result = await createGoalCompletion({
+      goalId,
+    })
+  }
+)
